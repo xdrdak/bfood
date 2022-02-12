80c9c310803e1ba71a6d3b3e911847851dc04a7e
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-02-12 10:45:10 -0500

    more tests

--- netlify/functions/post/post.js
+++ netlify/functions/post/post.js
@@ -1,25 +1,35 @@
 const { Client } = require("pg");
 
-const { PG_CONNECTION_STRING } = process.env;
+const { PG_CONNECTION_STRING, X_PASSWORD } = process.env;
 
 const client = new Client({
   connectionString: PG_CONNECTION_STRING,
 });
 
-exports.handler = async (event, context) => {
-  console.log(event, context);
+exports.handler = async (event) => {
+  const xPassword = event["x-password"];
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the e vent body encoded as a query string
+  if (!xPassword || xPassword !== X_PASSWORD) {
+    return { statusCode: 403, body: "Forbidden" };
+  }
+
   const params = new URLSearchParams(event.body);
-  const name = params.get("name") || "World";
+  const recipe = params.get("recipe");
+
+  if (!recipe || typeof recipe !== "string") {
+    return { statusCode: 400, body: "Bad Request. Missing recipe" };
+  }
+
+  await client.connect();
+  await client.query("INSERT INTO foodlist(DATA) values($1::text)", [recipe]);
+  await client.end();
 
   return {
     statusCode: 200,
-    body: `Hello, ${name}`,
+    body: `ok`,
   };
 };
